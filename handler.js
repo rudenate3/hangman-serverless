9d1d0481313ca7ada341fe51dd04d8dcd0c0d924
rudenate3 <nathan.odin@gmail.com>
2018-09-30 05:00:27 -0400

    Template working

--- handler.js
+++ handler.js
@@ -1,14 +1,48 @@
-'use strict';
+'use strict'
 
-module.exports.hello = async (event, context) => {
+const AWS = require('aws-sdk'),
+  docClient = new AWS.DynamoDB.DocumentClient({ region: process.env.REGION }),
+  uuid = require('uuid')
+
+module.exports.autoConfirmEmail = async (event, context, callback) => {
+  event.response.autoConfirmUser = true
+  callback(null, event)
+}
+
+module.exports.getGame = async (event, context, callback) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': process.env.ORIGIN
+    },
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+      message: 'Game Returned'
+    })
+  }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.addMove = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': process.env.ORIGIN
+    },
+    body: JSON.stringify({
+      message: 'Leaderboard returned',
+      input: event
+    })
+  }
+}
+
+module.exports.history = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': process.env.ORIGIN
+    },
+    body: JSON.stringify({
+      message: 'History returned',
+      input: event
+    })
+  }
+}
