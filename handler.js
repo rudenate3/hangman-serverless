c470cc63325f9b6f3b488fc187af1a7184916bfa
rudenate3 <nathan.odin@gmail.com>
2018-09-30 09:12:39 -0400

    history working

--- handler.js
+++ handler.js
@@ -95,15 +95,39 @@ module.exports.addMove = async (event, context) => {
   }
 }
 
-module.exports.history = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': process.env.ORIGIN
-    },
-    body: JSON.stringify({
-      message: 'History returned',
-      input: event
-    })
+module.exports.history = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    FilterExpression: 'email = :emailVal and gameOver = :gameOverVal',
+    ExpressionAttributeValues: {
+      ':emailVal': event.requestContext.authorizer.claims.email,
+      ':gameOverVal': true
+    }
   }
+
+  docClient.scan(params, (err, games) => {
+    if (err) {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': process.env.ORIGIN
+        },
+        body: JSON.stringify({
+          message: 'Error Returned',
+          error: err
+        })
+      })
+    }
+
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': process.env.ORIGIN
+      },
+      body: JSON.stringify({
+        message: 'Games Returned',
+        games: games.Items.length > 0 ? games.Items : 'None'
+      })
+    })
+  })
 }
